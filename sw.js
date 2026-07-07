8adb57e98448bbcd1aed1e7bd94855d932b50794
Gunsu Lee <83533586+lgswin@users.noreply.github.com>
2026-07-07 07:55:50 -0400

    cancel record

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pyoyeon-sucheop-v9';
+const CACHE = 'pyoyeon-sucheop-v11';
 const ASSETS = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
